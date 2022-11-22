b2d71e3b56fc13bb3e1a9e4ff128cb15548c020a
Davin <davin-dj@hotmail.com>
2022-11-21 21:43:13 -0500

    Added links to the packages page

---
+++ react-router/src/components/Package.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default function Package(props) {
+    return (
+        <div>{props.package}</div>
+    )
+}
--- react-router/src/components/Packages.js
+++ react-router/src/components/Packages.js
@@ -1,7 +1,17 @@
-
+import { Routes, Route, Link } from 'react-router-dom'
+import Package from './Package'
 
 export default function Packages(props) {
-    const displayPackages = props.packages.map((eachPackage) => <li>{eachPackage}</li>)
+
+    const displayPackages = props.packages.map((eachPackage, index) => {
+        return (
+            <li key={index}>
+                <Link to={`/packages/${eachPackage}`}>
+                    <Package package={eachPackage} />
+                </Link>
+            </li>
+        )
+    })
 
     return (
         <div>
@@ -9,9 +19,16 @@ export default function Packages(props) {
                 <div className="packagesHeader">
                     <h1>Our Packages</h1>
                 </div>
-                <ul>
-                    {displayPackages}
-                </ul>
+                <div>
+                    <ul>
+                        {displayPackages}
+                    </ul>
+                </div>
+            </div>
+            <div>
+                <Routes>
+                    <Route path='/packages/name' element={<Package />} />
+                </Routes>
             </div>
         </div>
     )
